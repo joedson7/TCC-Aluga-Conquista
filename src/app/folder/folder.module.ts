6c41d0b4a50c3487c8008106d111c386ba261f38
Filipe Souza <filipe.souza1602@gmail.com>
2021-12-10 00:11:17 -0300

    feat: criar tela de cadastrar imovel

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { CadastrarImovelComponent } from '../cadastrar-imovel/cadastrar-imovel.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    FolderPageRoutingModule
-  ],
-  declarations: [FolderPage]
+  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
+  declarations: [FolderPage, CadastrarImovelComponent],
 })
 export class FolderPageModule {}
