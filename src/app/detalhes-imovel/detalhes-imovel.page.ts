330544f0699f82c241c720a210bf7e3098153a05
Filipe Souza <filipe.souza1602@gmail.com>
2021-12-12 21:59:50 -0300

    exibir dados do imovel individual

--- src/app/detalhes-imovel/detalhes-imovel.page.ts
+++ src/app/detalhes-imovel/detalhes-imovel.page.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
+import { Imovel } from '../services/imoveis.service';
 
 @Component({
   selector: 'app-detalhes-imovel',
   templateUrl: './detalhes-imovel.page.html',
   styleUrls: ['./detalhes-imovel.page.scss'],
 })
-
 export class DetalhesImovelPage implements OnInit {
+  public imovel: Imovel;
+
   constructor(private modalCtrl: ModalController) {}
- 
 
   ngOnInit() {
+    console.log('imovel: ', this.imovel);
   }
+
   dismiss() {
     this.modalCtrl.dismiss();
   }
-
 }
