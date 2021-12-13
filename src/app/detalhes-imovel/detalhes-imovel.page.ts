9feb6cbf04e49566a4ddd7ae3b87aaf7779daf71
Joedson <joedson.7@hotmail.com>
2021-12-12 21:26:59 -0300

    css edits detalhesimovel

--- src/app/detalhes-imovel/detalhes-imovel.page.ts
+++ src/app/detalhes-imovel/detalhes-imovel.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-detalhes-imovel',
   templateUrl: './detalhes-imovel.page.html',
   styleUrls: ['./detalhes-imovel.page.scss'],
 })
-export class DetalhesImovelPage implements OnInit {
 
-  constructor() { }
+export class DetalhesImovelPage implements OnInit {
+  constructor(private modalCtrl: ModalController) {}
+ 
 
   ngOnInit() {
+  }
+  dismiss() {
+    this.modalCtrl.dismiss();
   }
 
 }
